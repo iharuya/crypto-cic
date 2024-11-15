ff7c8ec7b4b223b9b84b2877c87c698198121522
iharuya <94746057+iharuya@users.noreply.github.com>
2024-11-16 01:09:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Template
+
+- Bun
+- Vite
+  - React
+  - TypeScript
+  - SWC
+- Biome with some opinionated settings
+- Tailwind CSS
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const App: FC = () => {
+	return (
+		<main>
+			<p className="animate-bounce">hello</p>
+		</main>
+	);
+};
